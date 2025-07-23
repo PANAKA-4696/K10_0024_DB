eca2f6672c804244228869345c4d17c6e2770721
PANAKA-4696 <212013796+PANAKA-4696@users.noreply.github.com>
2025-07-23 14:19:12 +0900

    見た目のみ完成

---
+++ app/src/main/java/jp/ac/neec/it/k023c0024/k10_0024_db/ConfigActivity.kt
@@ -0,0 +1,14 @@
+package jp.ac.neec.it.k023c0024.k10_0024_db
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ConfigActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_config)
+    }
+}
